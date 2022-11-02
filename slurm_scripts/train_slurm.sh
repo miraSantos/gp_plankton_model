eb7e27e1e75d8f1de3cb8995ce6478214619af34
mira <miraflorista@gmail.com>
2022-11-02 11:35:15 -0400

    changed slurm script

--- slurm_scripts/train_slurm.sh
+++ slurm_scripts/train_slurm.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=train_gp_model    # Job name
 #SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=msantos@whoi.edu     # Where to send mail
-#SBATCH --array=1-9                  # how many tasks in the array
-#SBATCH --mem=30gb                     # Job memory request
+#SBATCH --ntasks=5                 # how many tasks in the array
+#SBATCH --mem_per_cpu=20gb                     # Job memory request
 #SBATCH --time=10:00:00               # Time limit hrs:min:sec
 #SBATCH --output=logs/train_%a_%j.log   # Standard output and error log
 pwd; hostname; date
@@ -16,7 +16,7 @@ echo "training model_spectral"
 
 cd /vortexfs1/scratch/msantos/gp_plankton_model/
 
-srun python -m  train.train $SLURM_ARRAY_TASK_ID
+srun --ntasks=5 -l --multi_prog slurm_scripts/slurm_config.conf
 echo "training finished"
 
 
