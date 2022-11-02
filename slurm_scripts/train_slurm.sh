ba14d1df446112957bb087dcacc5a79429a7bfd4
miraSantos <miras@mit.edu>
2022-11-02 12:46:04 -0400

    adding train_slurm

--- slurm_scripts/train_slurm.sh
+++ slurm_scripts/train_slurm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=train_gp_model    # Job name
-#SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=msantos@whoi.edu     # Where to send mail
-#SBATCH --ntasks=5                 # how many tasks in the array
-#SBATCH --mem-per-cpu=20gb                     # Job memory request
-#SBATCH --time=10:00:00               # Time limit hrs:min:sec
-#SBATCH --output=logs/train_%a_%j.log   # Standard output and error log
+#SBATCH --job-name=train_gp_model      # Job name
+#SBATCH --mail-type=ALL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=msantos@whoi.edu   # Where to send mail
+#SBATCH --ntasks=5                     # number of tasks
+#SBATCH --cpus-per-task=1              # cpus per task
+#SBATCH --mem-per-cpu=20gb             # memory per cpu
+#SBATCH --time=10:00:00                # Time limit hrs:min:sec
+#SBATCH --output=logs/train_%a_%j.log  # Standard output and error log
 pwd; hostname; date
 
 eval "$(conda shell.bash hook)"
@@ -16,8 +17,7 @@ echo "training model_spectral"
 
 cd /vortexfs1/scratch/msantos/gp_plankton_model/
 
-srun --ntasks=5 -l --multi-prog slurm_scripts/slurm_config.conf
+srun --ntasks=5 --cpus-per-task=1 --mem-per-cpu=20gb -l --multi-prog slurm_scripts/slurm_config.conf
 echo "training finished"
-
 
 date
