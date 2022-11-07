888df81a7fefb2547c64e4e329f6f3c7c9448354
miraSantos <miras@mit.edu>
2022-11-07 11:47:32 -0500

    adding changes to yaml file

--- slurm_scripts/sweep/sweep_slurm.sh
+++ slurm_scripts/sweep/sweep_slurm.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=sweep_gp_model    # Job name
 #SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=msantos@whoi.edu     # Where to send mail
-#SBATCH --ntasks=5                 # how many tasks in the array
-#SBATCH --mem-per-cpu=50G                     # Job memory request
+#SBATCH --ntasks=11                 # how many tasks in the array
+#SBATCH --mem-per-cpu=20G                     # Job memory request
 #SBATCH --time=10:00:00               # Time limit hrs:min:sec
 #SBATCH --output=logs/sweep_%a_%j.log   # Standard output and error log
 pwd; hostname; date
@@ -16,7 +16,7 @@ echo "training model_spectral"
 
 cd /vortexfs1/scratch/msantos/gp_plankton_model/
 
-srun --ntasks=5 --cpus-per-task=1 --mem-per-cpu=50G -l --multi-prog slurm_scripts/sweep/config.conf
+srun --ntasks=11 --cpus-per-task=1 --mem-per-cpu=20G -l --multi-prog slurm_scripts/sweep/config.conf
 echo "training finished"
 
 
