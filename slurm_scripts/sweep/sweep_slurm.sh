cba97f3ec7567709a6170f4e411d70098087e2b9
miraSantos <miras@mit.edu>
2022-11-07 12:05:46 -0500

    fixed sweep_slurm.sh

--- slurm_scripts/sweep/sweep_slurm.sh
+++ slurm_scripts/sweep/sweep_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=5                 # how many tasks in the array
 #SBATCH --mem-per-cpu=50G                     # Job memory request
 #SBATCH --time=10:00:00               # Time limit hrs:min:sec
-#SBATCH --output=logs/sweep_%a_%j.log   # Standard output and error log
+#SBATCH --output=/vortexfs1/scratch/msantos/gp_plankton_model/slurm_scripts/sweep/logs/sweep_%a_%j.log   # Standard output and error log
 pwd; hostname; date
 
 eval "$(conda shell.bash hook)"
@@ -18,6 +18,5 @@ cd /vortexfs1/scratch/msantos/gp_plankton_model/
 
 srun --ntasks=5 --cpus-per-task=1 --mem-per-cpu=50G -l --multi-prog slurm_scripts/sweep/config.conf
 echo "training finished"
-
 
 date
