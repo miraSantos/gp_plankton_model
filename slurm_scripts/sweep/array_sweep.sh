c68cba0f98d387d22fc4e2f0b3e3b33f0be4e98b
miraSantos <miras@mit.edu>
2022-11-09 22:15:51 -0500

    modified sweep_syn_config

--- slurm_scripts/sweep/array_sweep.sh
+++ slurm_scripts/sweep/array_sweep.sh
@@ -6,7 +6,7 @@
 #SBATHC --cpus-per-task=1
 #SBATCH --mem-per-cpu=20G                     # Job memory request
 #SBATCH --time=10:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/vortexfs1/scratch/msantos/gp_plankton_model/slurm_scripts/sweep/logs/array_sweep_%j.log   # Standard output and error log
+#SBATCH --output=/vortexfs1/scratch/msantos/gp_plankton_model/slurm_scripts/sweep/logs/array_sweep.log   # Standard output and error log
 pwd; hostname; date
 
 eval "$(conda shell.bash hook)"
@@ -19,9 +19,12 @@ cd /vortexfs1/scratch/msantos/gp_plankton_model
 
 wandb sweep --project syn_model_sweep cfg/sweep_syn_config.yaml 2> syn_temp.file
 
-for i in {1..4}; do
-  srun --ntasks=1 --tasks-per-cpu=1 --mem-per-cpu=20G eval "$(awk 'NR==4 {print $6, $7, $8}' syn_temp.file)"
-  done
+cat syn_temp.file
+
+
+#for i in {1..5}; do
+#  eval "$(awk 'NR==4 {print $6, $7, $8}' syn_temp.file)" &
+#  done
 
 echo "sweep finished"
 
