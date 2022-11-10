be4ab47c49e6cd1ff641ef92dde38f24a338c1d0
miraSantos <miras@mit.edu>
2022-11-10 01:10:04 -0500

    changed yaml files, updated array_sweep.sh

--- slurm_scripts/sweep/array_sweep.sh
+++ slurm_scripts/sweep/array_sweep.sh
@@ -2,9 +2,7 @@
 #SBATCH --job-name=array_sweep_gp_model    # Job name
 #SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=msantos@whoi.edu     # Where to send mail
-#SBATCH --array=1-5               # how many tasks in the array
-#SBATHC --cpus-per-task=1
-#SBATCH --mem-per-cpu=20G                     # Job memory request
+#SBATCH --mem=50G                     # Job memory request
 #SBATCH --time=10:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/vortexfs1/scratch/msantos/gp_plankton_model/slurm_scripts/sweep/logs/array_sweep.log   # Standard output and error log
 pwd; hostname; date
@@ -17,15 +15,12 @@ echo "training model_spectral"
 
 cd /vortexfs1/scratch/msantos/gp_plankton_model
 
-wandb sweep --project syn_model_sweep cfg/sweep_syn_config.yaml 2> syn_temp.file
+wandb sweep --project syn_model_sweep cfg/sweep_config.yaml 2> temp.file
 
-cat syn_temp.file
-
-
-#for i in {1..5}; do
-#  eval "$(awk 'NR==4 {print $6, $7, $8}' syn_temp.file)" &
-#  done
-
+for i in {1..5}; do
+  eval "$(awk 'NR==4 {print $6, $7, $8}' temp.file)" &
+  done
+wait
 echo "sweep finished"
 
 date
