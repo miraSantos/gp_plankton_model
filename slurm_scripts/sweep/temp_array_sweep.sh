f13e8313b82ec50a045dd347e9331d310656854e
mira <miraflorista@gmail.com>
2022-11-10 02:56:39 -0500

    added new array sweep and config files for other taxa and temp

---
+++ slurm_scripts/sweep/temp_array_sweep.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=array_sweep_gp_model    # Job name
+#SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=msantos@whoi.edu     # Where to send mail
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=180GB                     # Job memory request
+#SBATCH --time=10:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/vortexfs1/scratch/msantos/gp_plankton_model/slurm_scripts/sweep/logs/syn_array_sweep.log   # Standard output and error log
+pwd; hostname; date
+
+eval "$(conda shell.bash hook)"
+
+conda activate gpytorch
+
+echo "training model_spectral"
+
+cd /vortexfs1/scratch/msantos/gp_plankton_model
+
+wandb sweep --project syn_model_sweep cfg/sweep_temp_config.yaml 2> temp.file
+
+cat temp.file
+
+for i in {1}; do
+  eval "$(awk 'NR==4 {print $6, $7, $8}' temp.file)" &
+  done
+wait
+echo "sweep finished"
+
+date
